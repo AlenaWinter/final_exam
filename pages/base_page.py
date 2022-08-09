444ca2cd914abcb5a1d12d820fcf5a4e105f2dab
AlenaWinter <110168622+AlenaWinter@users.noreply.github.com>
2022-08-09 16:09:46 +0500

    Задание: добавление в корзину со страницы товара

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,32 @@
-class BasePage():
-     def __init__(self, browser, url): #метод, который вызывается, когда мы создаем объект. Конструктор объявляется ключевым словом __init__. В него в качестве параметров мы передаем экземпляр драйвера и url адрес. Внутри конструктора сохраняем эти данные как аттрибуты нашего класса
-         self.browser = browser
-         self.url = url
-     def open(self): #метод open. Он должен открывать нужную страницу в браузере, используя метод get()
-         self.browser.get(self.url)
+from selenium.common.exceptions import NoSuchElementException    # Импорт исключений
+from selenium.common.exceptions import NoAlertPresentException
+import math
+
+class BasePage(object):
+    def __init__(self, browser, url, timeout=100):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)    # Команда для неявного ожидания в течении timeout сек.
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            print("Your code: {}".format(alert.text))
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
