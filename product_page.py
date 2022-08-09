3e313a323f948b50959e469e3b86d1752cc01bde
AlenaWinter <110168622+AlenaWinter@users.noreply.github.com>
2022-08-09 16:08:10 +0500

    Delete product_page.py

--- product_page.py
+++
@@ -1,9 +0,0 @@
-﻿from .locators import ProductPageLocators    # Импорт класса, описывающего главную страницу
-from .base_page import BasePage            # Импорт базового класса BasePage
-
-
-class ProductPage(BasePage):
-    def add_product_to_cart(self):
-        link = self.browser.find_element(*ProductPageLocators.BASKET_BTN)
-        link.click()
-        self.solve_quiz_and_get_code()
