a7263c715a1428dcdb6d775e21070fa762fe7d04
AlenaWinter <110168622+AlenaWinter@users.noreply.github.com>
2022-08-09 16:09:16 +0500

    Add files via upload
    Задание: добавление в корзину со страницы товара

---
+++ test_product_page.py
@@ -0,0 +1,10 @@
+﻿from .pages.product_page import ProductPage
+
+
+class TestAddProductToBasket:
+    def test_guest_can_add_product_to_cart(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_product_to_cart()
+        # pytest -s test_product_page.py
