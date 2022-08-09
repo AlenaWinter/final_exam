26cecb7d10f5bfd8172132dd634c2ee282dec6f6
AlenaWinter <110168622+AlenaWinter@users.noreply.github.com>
2022-08-09 16:05:34 +0500

    Задание: добавление в корзину со страницы товара

---
+++ main_page.py
@@ -0,0 +1,12 @@
+from .base_page import BasePage #импорт базового класса BasePage
+from selenium.webdriver.common.by import By
+from .login_page import LoginPage
+from .locators import MainPageLocators #импортируем MainPageLocators из файла locators.py
+class MainPage(BasePage): # класс  MainPage: Его нужно сделать наследником класса BasePage. Класс-предок в Python указывается в скобках
+    def go_to_login_page(self):    # Создание метода, позволяющего попасть на страницу авторитизации
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+        return LoginPage(browser=self.browser, url=self.browser.current_url) #проинициализировать новый объект Page и вернуть его
+    def should_be_login_link(self):
+         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented" # осмысленное сообщение об ошибке
+    
