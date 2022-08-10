248a6cfecd4098b5dc8fa327c3f4b157087ed354
AlenaWinter <110168622+AlenaWinter@users.noreply.github.com>
2022-08-10 11:55:36 +0500

    Плюсы наследования: пример

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,6 +3,7 @@ from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
+from .locators import BasePageLocators
 import math
 
 class BasePage(object):
@@ -11,8 +12,17 @@ class BasePage(object):
         self.url = url
         self.browser.implicitly_wait(timeout)    # Команда для неявного ожидания в течении timeout сек.
 
-    def open(self):
-        self.browser.get(self.url)
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+                until_not(expected_conditions.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
 
     def is_element_present(self, how, what):
         try:
@@ -20,13 +30,20 @@ class BasePage(object):
         except (NoSuchElementException):
             return False
         return True
+
     def is_not_element_present(self, how, what, timeout=4):
         try:
             WebDriverWait(self.browser, timeout).until(expected_conditions.presence_of_element_located((how, what)))
         except TimeoutException:
             return True
         return False
-        
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
@@ -39,3 +56,4 @@ class BasePage(object):
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+            
